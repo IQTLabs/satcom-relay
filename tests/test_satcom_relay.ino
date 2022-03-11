723dcf1a408054487d0f0a85913ee1437f5be291
Josh Bailey <josh@vandervecken.com>
2022-03-11 16:42:56 +1300

    test iridium class.

--- tests/test_satcom_relay.ino
+++ tests/test_satcom_relay.ino
@@ -3,21 +3,33 @@
 #include <Arduino.h>
 #include <AUnit.h>
 
+#define PIO_SERCOM 0
+#define pinPeripheral(_x, _y) { (void)_x; (void)_y; }
+
+
 class Uart {
   public:
-    Uart() {};
+    Uart() { clear(); };
+    void begin(int _baud) { (void)_baud; };
     bool available() { return p < l; }
     char read() { return buffer[p++]; }
-    void setbuf(const char *test_str) { buffer = test_str; l = strlen(test_str); p = 0; };
-    byte p = 0;
-    byte l = 0;
-    const char *buffer = NULL;
+    void setbuf(const char *test_str) { clear(); buffer = test_str; l = strlen(test_str); }
+    void clear() { p = 0; l = 0; w = 0; buffer = NULL; }
+    int write(const uint8_t &s) { writeBuffer[w++] = s; return 1; }
+    int write(const uint8_t *s, int c) { for (byte i = 0; i < c; ++i) { write(s[i]); } return c; }
+    void println() { write('\n'); }
+    byte p;
+    byte l;
+    byte w;
+    const char *buffer;
+    char writeBuffer[255] = {0};
 };
 
 Uart uart;
 
 #include "../satcom-relay/timediff.h"
 #include "../satcom-relay/sensor-manager.h"
+#include "../satcom-relay/iridium-modem.h"
 
 using aunit::TestRunner;
 
@@ -25,8 +37,19 @@ const char okJson[] = "{\"D\": 1}\n";
 const char okHbJson[] = "{\"D\": 1, \"H\": 1}\n";
 const char badJson[] = "{/notJson\n";
 
+test(iridium, check) {
+  uart.clear();
+  IridiumModem modem;
+  modem.begin(&uart, 0, 0, 0);
+  modem.check();
+  DynamicJsonDocument doc(256);
+  doc["test"] = "test";
+  modem.sendJSON(doc);
+}
+
 test(parse, check) {
   bool isHeartbeat = false;
+  uart.clear();
   DynamicJsonDocument doc(256);
   SensorSerialManager ssm(&uart, &doc);
   assertEqual(false, ssm.poll());
